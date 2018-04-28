7efbdde2f9e3a9df32122a199bac478490860074
Andy <anlaics2@gmail.com>
2018-04-28 13:05:27 -0700

    ui change

--- src/components/AddCoinScreen.js
+++ src/components/AddCoinScreen.js
@@ -1,43 +1,60 @@
 import React, { Component } from 'react';
-import { Text, View, Picker } from 'react-native';
+import { Text, View, Picker, ScrollView } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import { LinearGradient } from 'expo';
 import { CardSection } from './common';
 import { Button, Card } from 'react-native-elements';
+import { addCoin } from '../actions';
+import AddCoinDetail from './AddCoinDetail';
 
 class AddCoinScreen extends Component {
+    state = { coins: [] };
+    componentWillMount() {
+        // ASYNC HTTP Request to get coins from the API.
+        fetch('https://api.coinmarketcap.com/v1/ticker/?limit=200')
+            .then((response) => response.json())
+            .then((responseData) => this.setState({ coins: responseData }));
+    }
+
+    // Render all the coins that was fetched from the API.
+    renderCoins() {
+        return this.state.coins.map(coin =>
+            <AddCoinDetail key={coin.name} coinProp={coin} />);
+        //coinProp variable can be named anything as long as we use that name in other functions
+    }
     render() {
         return (
             <View style={styles.viewContainer}>
                 <LinearGradient
                     colors={['#452768', '#171032', '#04081B']}>
-                    <Card
-                        containerStyle={styles.cardContainer}
-                    >
-                        <View style={{ backgroundColor: "#FFF", padding: 10, alignItems: "center" }}>
-                            <Text style={{fontWeight: 'bold' }}>
-                                Pick a Coin
+                    <ScrollView>
+                        <Card
+                            containerStyle={styles.cardContainer}
+                        >
+                            <View style={{ backgroundColor: "#23213F", padding: 10, alignItems: "center" }}>
+                                <Text style={{ fontWeight: 'bold', color: '#FFF' }}>
+                                    Pick Coin(s)
                             </Text>
-                            <Picker>
-                            </Picker>
-                        </View>
-                    </Card>
-                    <CardSection>
-                        <Button
-                            onPress={() => Actions.addCoin()}
-                            title="Add Coin "
-                            titleStyle={{ fontWeight: 'bold' }}
-                            buttonStyle={{
-                                backgroundColor: "rgba(92, 99,216, 1)",
-                                width: 300,
-                                height: 45,
-                                borderColor: "transparent",
-                                borderWidth: 0,
-                                borderRadius: 5
-                            }}
-                            containerStyle={{ marginTop: 20 }}
-                        />
-                    </CardSection>
+                            </View>
+                        </Card>
+                        {this.renderCoins()}
+                        <CardSection>
+                            <Button
+                                onPress={() => this.props.addCoin()}
+                                title="Add Coin "
+                                titleStyle={{ fontWeight: 'bold' }}
+                                buttonStyle={{
+                                    backgroundColor: "rgba(92, 99,216, 1)",
+                                    width: 300,
+                                    height: 45,
+                                    borderColor: "transparent",
+                                    borderWidth: 0,
+                                    borderRadius: 5
+                                }}
+                                containerStyle={{ marginTop: 20 }}
+                            />
+                        </CardSection>
+                    </ScrollView>
                 </LinearGradient>
             </View>
         );
